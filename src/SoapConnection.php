bd995a022bc49f4d48980182d3fe1f267770e9d1
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-06-18 18:30:27 +0430

    Complete Soap Connection with test.

--- src/SoapConnection.php
+++ src/SoapConnection.php
@@ -18,15 +18,88 @@ class SoapConnection
         if (empty($this->connection)) {
             try {
                 $params = array(
-                    'login' => $connect['username'],
-                    'password' => $connect['password'], // Credientials
-                    'features' => SOAP_USE_XSI_ARRAY_TYPE // Required
-                                );
-                $this->connection = new SoapClient($connect['wsdl']);
+                    'trace' => true,
+                    'exceptions' => true,
+                    'compression' => SOAP_COMPRESSION_ACCEPT,
+                    'connection_timeout' => 120,
+                    'cache_wsdl' => WSDL_CACHE_BOTH
+                );
+                $this->connection = new SoapClient($connect['wsdl'], $params);
             } catch (SoapFault $e) {
                 throw new AsanakSOAPException($e->faultstring, 401);
             }
         }
         return $this->connection;
+    }
+
+    public function sendSms()
+    {
+        try {
+            $s = $this->connection->sendSms(array(
+                'userCredential' => array(
+                    'username' => $this->datasets['username'],
+                    'password' => $this->datasets['password']
+                ),
+                'srcAddresses' => $this->datasets['srcAddresses'],
+                'destAddresses' => $this->datasets['destAddresses'],
+                'msgBody' => $this->datasets['msgBody'],
+                'msgEncoding' => $this->datasets['msgEncoding']
+            ));
+            return $s;
+        } catch (SoapFault $e) {
+            throw new AsanakSOAPException($e->faultstring, 401);
+        }
+    }
+
+    public function getReportByMsgId()
+    {
+        try {
+            $s = $this->connection->getReportByMsgId(array(
+                'userCredential' => array(
+                    'username' => $this->datasets['username'],
+                    'password' => $this->datasets['password']
+                ),
+                'msgIds' => $this->datasets['msgIds']
+            ));
+            return $s;
+        } catch (SoapFault $e) {
+            throw new AsanakSOAPException($e->faultstring, 401);
+        }
+    }
+
+    public function getReceivedMsg($values)
+    {
+        try {
+            $s = $this->connection->getReceivedMsg(array(
+                'userCredential' => array(
+                    'username' => $values['username'],
+                    'password' => $values['password']
+                ),
+                'wsdl' => $values['wsdl'],
+                'srcAddresses' => $values['srcAddresses'],
+                'destAddresses' => $values['destAddresses'],
+                'maxReturnedMsg' => $values['maxReturnedMsg'],
+                'fromTime' => $values['fromTime']
+            ));
+            return $s;
+        } catch (SoapFault $e) {
+            throw new AsanakSOAPException($e->faultstring, 401);
+        }
+    }
+    
+    public function getUserCredit($values)
+    {
+        try {
+            $s = $this->connection->getUserCredit(array(
+                'userCredential' => array(
+                    'username' => $values['username'],
+                    'password' => $values['password']
+                ),
+                'wsdl' => $values['wsdl']
+            ));
+            return $s;
+        } catch (SoapFault $e) {
+            throw new AsanakSOAPException($e->faultstring, 401);
+        }
     }
 }
