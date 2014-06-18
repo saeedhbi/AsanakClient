9de01c8102132ceed73d426fddd265655dcb59f5
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-06-18 13:34:59 +0430

    Test objects throw for connect method.

--- spec/SoapConnectionSpec.php
+++ spec/SoapConnectionSpec.php
@@ -11,4 +11,52 @@ class SoapConnectionSpec extends ObjectBehavior
     {
         $this->shouldHaveType('SoapConnection');
     }
+    
+    function let(){
+        $messgae = 'Asanak Test';
+        $Encoding = (mb_detect_encoding($messgae) == 'ASCII') ? "1" : "8";
+        $values = array(
+            'username' => 'opilo',
+            'password' => 'opilo@7800',
+            'wsdl' => 'http://ws.asanak.ir:8082/services/CompositeSmsGateway?wsdl',
+            'connectionType'=>'soap',
+            'srcAddresses' =>'9821021000',
+            'destAddresses'=> '09354328595',
+            'msgBody' => $messgae,
+            'msgEncoding' => $Encoding
+        );
+        $this->beConstructedWith($values);
+    }
+    
+    function it_should_connect_to_wsdl_with_error(){
+        $messgae = 'Asanak Test';
+        $Encoding = (mb_detect_encoding($messgae) == 'ASCII') ? "1" : "8";
+        $values = array(
+            'username' => 'opilo',
+            'password' => 'opilo@7800',
+            'wsdl' => 'http1://ws.asanak.ir:8082/services/CompositeSmsGateway?wsdl',
+            'connectionType'=>'soap',
+            'srcAddresses' =>'9821021000',
+            'destAddresses'=> '09354328595',
+            'msgBody' => $messgae,
+            'msgEncoding' => $Encoding
+        );
+        $this->shouldThrow('Exceptions\AsanakSOAPException')->during('connect', array($values));
+    }
+   
+    function it_should_return_object_of_soap_client(){
+        $messgae = 'Asanak Test';
+        $Encoding = (mb_detect_encoding($messgae) == 'ASCII') ? "1" : "8";
+        $values = array(
+            'username' => 'opilo',
+            'password' => 'opilo@7800',
+            'wsdl' => 'http://ws.asanak.ir:8082/services/CompositeSmsGateway?wsdl',
+            'connectionType'=>'soap',
+            'srcAddresses' =>'9821021000',
+            'destAddresses'=> '09354328595',
+            'msgBody' => $messgae,
+            'msgEncoding' => $Encoding
+        );
+        $this->connect($values)->shouldBeObject();
+    }
 }
--- src/SoapConnection.php
+++ src/SoapConnection.php
@@ -1,6 +1,32 @@
 <?php
+use Exceptions\AsanakSOAPException;
 
 class SoapConnection
 {
-    
+
+    private $datasets = array();
+
+    private $connection;
+
+    public function __construct($values)
+    {
+        $this->datasets = $values;
+    }
+
+    public function connect($connect)
+    {
+        if (empty($this->connection)) {
+            try {
+                $params = array(
+                    'login' => $connect['username'],
+                    'password' => $connect['password'], // Credientials
+                    'features' => SOAP_USE_XSI_ARRAY_TYPE // Required
+                                );
+                $this->connection = new SoapClient($connect['wsdl']);
+            } catch (SoapFault $e) {
+                throw new AsanakSOAPException($e->faultstring, 401);
+            }
+        }
+        return $this->connection;
+    }
 }
